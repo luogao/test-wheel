ca58b6e74a6a0be0880a67030516a749ae49bc28
Roy <gao.luo@honjoinfo.com>
2017-12-15 11:27:08 +0800

    添加新的代码方法

--- src/components/dynamicList.vue
+++ src/components/dynamicList.vue
@@ -193,6 +193,27 @@
                     </code>
                 </pre>
             </v-card>
+            <v-card class="mt25">
+                <v-toolbar color="dark" dark>
+                    <v-toolbar-side-icon></v-toolbar-side-icon>
+                    <v-toolbar-title>点击非目标区域触发事件</v-toolbar-title>
+                </v-toolbar>
+                <pre>
+                    <code class="language-javascript">
+                        $(document).mouseup(function(e){
+                        var _con = $(' 目标区域 ');   // 设置目标区域
+                        if(!_con.is(e.target) && _con.has(e.target).length === 0){ // Mark 1
+                            some code...   // 功能代码
+                        }
+                        });
+                        /* Mark 1 的原理：
+                        判断点击事件发生在区域外的条件是：
+                        1. 点击事件的对象不是目标区域本身
+                        2. 事件对象同时也不是目标区域的子元素
+                        */
+                    </code>
+                </pre>
+            </v-card>
         </v-flex>
     </v-layout>
 </template>
